738311fed18ecc5e7f830b79aad9c22a297e9529
NrMagic <wm_araujo@hotmail.com>
2026-03-26 11:14:01 -0300

    android-studio

---
+++ android-aula-one/aula-one/src/constants/style.js
@@ -0,0 +1,27 @@
+import { Platform, statusBar} from 'react-native'
+
+export const prioridade ={
+    Alta: '#f00',
+    Media: '#ff9800',
+    Baixa: '#00b800',
+    }
+
+export default STATUS_BAR_ HEIGHT = Platform.OS ===
+'android' ? StatusBar.currnHeight : 0
+
+export const Primay_Color = '#6200ee'
+export const Accent_Color_Yelllow = '#ffc107'
+export const Accent_Color_Red = '#f44336'
+
+export const Text_Color_Dark = '#333'
+export const Text_Color_Light = "#fff"
+export const Text_Color_Medium = '#555'
+export const Text_Color_Disable ='#aaa'
+
+export const Background_Color_light = '#f5f5f5'
+export const Background_Color_White = '#fff'
+
+export const Border_Color_Light = '#ccc'
+
+export const Background_Color_Input ='#f9f9f9'
+export const Overlay_Color = 'rgba(0, 0, 0, 0,)'
---
+++ android-aula-one/aula-one/src/styles/HomeScreenStyles.js
@@ -0,0 +1,4 @@
+import {platform, StatusBar } from 'react-native'
+import{
+
+}
